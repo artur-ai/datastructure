720e081829e6e3583e9785996057c345df71943d
artur <mauboroda.arturtu@gmail.com>
2025-01-31 12:14:42 +0200

    Did all methods and tests

--- src/main/com/maiboroda.list/ArrayList.java
+++ src/main/com/maiboroda.list/ArrayList.java
@@ -42,37 +42,77 @@ public class ArrayList implements List {
 
         @Override
         public Object set(Object value, int index) {
+            list.add("A");
+            list.add("B");
+            list.add("C");
+            list.add("D");
+            list.set(0, Integer.parseInt("E"));
+            System.out.println(list);
 
             return null;
         }
 
         @Override
         public void clear() {
+            list.add("Banana");
+            list.add("Apple");
+            list.add("Cherry");
+            System.out.println("ArrayList: " + list);
+            System.out.println(list.size());
+
+            list.clear();
+            System.out.println("ArrayList: " + list);
+            System.out.println(list.size());
 
         }
 
         @Override
         public int size() {
+            list.add("Banana");
+            list.add("Apple");
+            list.add("Cherry");
+            System.out.println(list.size());
+
+
             return 0;
         }
 
         @Override
         public boolean isEmpty() {
+            System.out.println("list is empty? " + list.isEmpty());
+            list.add("Banana");
+            System.out.println("List is empty? " + list.isEmpty());
             return false;
         }
 
         @Override
         public boolean contains(Object value) {
+            list.add("Banana");
+            list.add("Apple");
+            list.add("Cherry");
+
+            System.out.println("Does the list include banana? " + list.contains(new String("Banana")));
+            System.out.println("Does the list include Pear? " + list.contains(new String("Pear")));
             return false;
         }
 
         @Override
         public int indexOf(Object value) {
+            list.add("Banana");
+            list.add("Apple");
+            list.add("Cherry");
+
+            System.out.println("Index of banana is " + list.indexOf("Banana"));
+            System.out.println("Index of Pear is " + list.indexOf("Pear"));
             return 0;
         }
 
         @Override
         public int lastIndexOf(Object value) {
+            list.add("Banana");
+            list.add("Apple");
+            list.add("Cherry");
+            System.out.println("Index of Cherry is " + list.lastIndexOf("Cherry"));
             return 0;
         }
     }
--- src/test/test/main/com/maiboroda/list/ArrayListTest.java
+++ src/test/test/main/com/maiboroda/list/ArrayListTest.java
@@ -38,8 +38,6 @@ public class ArrayListTest {
         list.add("C");
         list.remove(0);
         assertEquals(list.remove(0), "B");
-
-
     }
 
     @Test
@@ -53,8 +51,75 @@ public class ArrayListTest {
 
     @Test
     public void setValueinList() {
+        list.add("Java");
+        list.add("C++");
+        list.add("Python");
+        list.add("C#");
+        list.set(1, "Java Script");
 
+        assertEquals(list.set(1, "C++"), "Java Script");
     }
+
+    @Test
+    public void clearList() {
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+        assertEquals(list.size(), 3);
+
+        list.clear();
+        assertEquals(list.size(), 0);
+    }
+
+    @Test
+    public void listSize() {
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+        System.out.println(list.size());
+        assertEquals(list.size(), 3);
+    }
+
+    @Test
+    public void listEmpty() {
+        System.out.println("list is empty? " + list.isEmpty());
+        list.add("Banana");
+        System.out.println("List is empty? " + list.isEmpty());
+
+        assertEquals(list.size(), 1);
+    }
+
+    @Test
+    public void listContains() {
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+
+        System.out.println("Does the list include banana? " + list.contains(new String("Banana")));
+        assertEquals(list.contains("Banana"), true);
+    }
+
+    @Test
+    public void indexOfArrayElements() {
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+
+        System.out.println("Index of banana is " + list.indexOf("Banana"));
+        assertEquals(list.indexOf("Banana"), 0);
+    }
+
+    @Test
+    public void lastIndexOfArrayElements() {
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Cherry");
+        System.out.println("Index of Cherry is " + list.lastIndexOf("Cherry"));
+        assertEquals(list.lastIndexOf("Cherry"), 2);
+    }
+
+
+
 
 
 }
