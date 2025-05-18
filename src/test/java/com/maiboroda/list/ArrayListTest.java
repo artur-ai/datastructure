b5b2ce688eb5388233958f5a5c25d9cb1c595bf7
ValentynaBondarenko <valentynaBondarenkoo@gmail.com>
2025-05-18 21:03:40 +0300

    Enhancement: Improve list structure and testing framework
    - Updated List interface to extend Iterable for iterator support
    - Modified ArrayList implementation accordingly
    - Adjusted pom.xml dependencies
    - Renamed ListTest to AbstractListTest to better reflect its abstract nature

--- src/test/java/com/maiboroda/list/ArrayListTest.java
+++ src/test/java/com/maiboroda/list/ArrayListTest.java
@@ -1,31 +1,9 @@
 package com.maiboroda.list;
 
-import org.junit.jupiter.api.Test;
-
-public class ArrayListTest<E> extends ListTest<E> {
-
-    @Test
-    void test() {
-        testAddElemntToList();
-        testAddByIndexElementsToLList();
-        testRemoveElmentsFromLList();
-        testClearSizeAndIsEmptyInList();
-        testContainsOfValueInList();
-        testGetElementsFromList();
-        testIndexOfValueInList();
-        testLastIndexOfValueInList();
-        testSetValueInlist();
-        addValueBiggerThanSize();
-        addObject_returnStringInList();
-        getValueWithNegativeAndBigIndex();
-        removeValueWithNegativeAndBigIndex();
-        setValueWithNegativeAndBigIndex();
-        shouldSendExcrptionWhenUseNegativeIndex();
-    }
-
+public class ArrayListTest extends AbstractListTest {
     @Override
-    protected List<String> creatList() {
-        return new ArrayList<String>();
+    List<String> getList() {
+        return new ArrayList<>();
     }
 }
 
--- src/test/java/com/maiboroda/list/LinkedListTest.java
+++ src/test/java/com/maiboroda/list/LinkedListTest.java
@@ -1,50 +1,10 @@
 package com.maiboroda.list;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.Iterator;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class LinkedListTest<E> extends ListTest<E> {
+public class LinkedListTest extends AbstractListTest {
 
     @Override
-    protected List<String> creatList() {
-        return new LinkedList<String>();
+    protected List<String> getList() {
+        return new LinkedList<>();
     }
 
-    @Test
-    void test() {
-        testAddElemntToList();
-        testAddByIndexElementsToLList();
-        testRemoveElmentsFromLList();
-        testClearSizeAndIsEmptyInList();
-        testContainsOfValueInList();
-        testGetElementsFromList();
-        testIndexOfValueInList();
-        testLastIndexOfValueInList();
-        testSetValueInlist();
-        addObject_returnStringInList();
-    }
-
-    @Test
-    void testIteratorInLinkedList() {
-        LinkedList list = new LinkedList();
-        list.add("Java");
-        list.add("C++");
-        list.add("C#");
-
-        Iterator<String> iterator = list.iterator();
-
-        assertTrue(iterator.hasNext());
-        assertEquals("Java", iterator.next());
-
-        assertTrue(iterator.hasNext());
-        assertEquals("C++", iterator.next());
-
-        assertTrue(iterator.hasNext());
-        assertEquals("C#", iterator.next());
-
-        assertFalse(iterator.hasNext());
-    }
 }
